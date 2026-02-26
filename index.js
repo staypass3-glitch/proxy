f19e7d0235fee825c6689d696c6b2f912301b1ba
guru bhamare <staypass3@gmail.com>
2026-02-26 09:16:50 +0530

    Third commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const SUPABASE_URL = 'https://japrxgqsdstohjbujbxw.supabase.co';
   const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImphcHJ4Z3FzZHN0b2hqYnVqYnh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExMTA1MjAsImV4cCI6MjA1NjY4NjUyMH0.ScS0IJkn3s5fwMQMSK1O_AFriEBkxAcIVjzvCjTZBh4';
 
-  const response = await fetch(`${SUPABASE_URL}${req.url.replace('/api','')}`, {
+  const response = await fetch(`${SUPABASE_URL}${req.url.replace('/','')}`, {
     method: req.method,
     headers: {
       'apikey': SUPABASE_KEY,
